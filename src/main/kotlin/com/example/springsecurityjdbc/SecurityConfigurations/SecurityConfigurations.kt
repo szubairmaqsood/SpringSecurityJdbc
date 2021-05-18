877f902d7e26a4cb3bc23a9ceec71e866663e510
zubair-pc\zubair <szubair@gmail.com>
2021-05-18 13:32:07 +0500

    1.Basic strcuture is there 2.Basic controller named as home reasource is made 3.In application properties required values to connect to database are mentioned 4.In security configuration authorization are mentioned

---
+++ src/main/kotlin/com/example/springsecurityjdbc/SecurityConfigurations/SecurityConfigurations.kt
@@ -0,0 +1,61 @@
+package com.example.springsecurityjdbc.SecurityConfigurations
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import javax.sql.DataSource
+
+@EnableWebSecurity
+class SecurityConfigurations:WebSecurityConfigurerAdapter {
+
+    var dataSource: DataSource;
+
+    @Autowired
+    constructor(_dataSource: DataSource)
+    {
+        this.dataSource = _dataSource
+    }
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth
+                ?.jdbcAuthentication()
+                ?.dataSource(this.dataSource)
+              
+                ?.withUser(
+                        User.withUsername("user")
+                                .password("pass")
+                                .roles("USER")
+                )
+                ?.withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
+    }
+
+    /*
+     This method is related to Authorization
+     */
+    override fun configure(http: HttpSecurity?) {
+        http
+                ?.authorizeRequests()
+                ?.antMatchers("/admin")?.hasRole("ADMIN")
+                ?.antMatchers("/user")?.hasAnyRole("ADMIN","USER")
+                ?.antMatchers("/")?.permitAll()
+                ?.and()
+                ?.formLogin()
+    }
+
+    /*
+     Password Encoder
+     */
+    @Bean
+    fun getPasswordEncoder():PasswordEncoder{
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/kotlin/com/example/springsecurityjdbc/SpringSecurityJdbcApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsecurityjdbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityJdbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityJdbcApplication>(*args)
+}
